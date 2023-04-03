277eb4ebf57805abee14ad24de0a459855237375
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-04-03 16:25:20 -0300

    Erros de lint corrigidos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,12 @@ import mealIcon from '../images/mealIcon.svg';
 
 export default function Footer() {
   return (
-    <div data-testid="footer" className='footer'>
+    <div data-testid="footer" className="footer">
       <Link to="/drinks">
-        <img src={drinkIcon} data-testid="drinks-bottom-btn"/>
+        <img src={ drinkIcon } data-testid="drinks-bottom-btn" alt="Icone de bebidas" />
       </Link>
       <Link to="/meals">
-        <img src={mealIcon} data-testid="meals-bottom-btn"/>
+        <img src={ mealIcon } data-testid="meals-bottom-btn" alt="Icone de carnes" />
       </Link>
     </div>
   );
