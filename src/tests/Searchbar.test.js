ed304be6e94618d8bd1a502f952db3b2a3b12a11
Carol Mendes <anacpersonal@gmail.com>
2023-04-04 15:01:48 -0700

    testes-corrigido

--- src/tests/Searchbar.test.js
+++ src/tests/Searchbar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { act, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import { act, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import LoginProvider from '../context/LoginContext';
